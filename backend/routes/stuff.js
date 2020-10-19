202002e5237cfdd3459409fe23439a7c112feda7
ronan pradot <ronan.pradot@hotmail.fr>
2020-10-19 18:09:31 +0200

    middleware.multer

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const stuffCtrl = require('../controllers/stuff');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-router.post('/', stuffCtrl.createSauce);
-router.put('/:id', stuffCtrl.modifySauce);
-router.delete('/:id', stuffCtrl.deleteSauce);
-router.get('/:id', stuffCtrl.getOneSauce);
-router.get('/', stuffCtrl.getAllSauce);
+router.post('/', auth, multer, stuffCtrl.createSauce);
+router.put('/:id', auth, multer, stuffCtrl.modifySauce);
+router.delete('/:id', auth, multer, stuffCtrl.deleteSauce);
+router.get('/:id', auth, multer, stuffCtrl.getOneSauce);
+router.get('/', auth, multer, stuffCtrl.getAllSauce);
 
 module.exports = router;
