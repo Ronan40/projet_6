6446fedbd81845ad6c2d46e7d033999049919587
ronan pradot <ronan.pradot@hotmail.fr>
2020-10-20 14:54:22 +0200

    like/dislike

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use('/api/stuff', stuffRoutes);
+app.use('/api/sauces', stuffRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- backend/models/sauce.js
+++ backend/models/sauce.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 
-// Modèle du schema :
+// Modèle du schema Mongoose :
 
 const sauceSchema = mongoose.Schema({
-    userId: { type: String, required: true },
-    name: { type: String, required: true, maxlength: [15, 'Nombre de caractéres limité a 15'] },
-    manufacturer: { type: String, required: true, maxlength: [30, 'Nombre de caractéres limité a 30'] },
-    description: { type: String, required: true },
-    mainPepper: { type: String, required: true, maxlength: [15, 'Nombre de caractéres limité a 15'] },
-    imageUrl: { type: String, required: true },
-    heat: { type: Number, required: true },
-    likes: { type: Number, required: false, default: 0 },
-    dislikes: { type: Number, required: false, default: 0 },
-    usersLiked: { type: [String], required: false },
-    usersDisliked: { type: [String], required: false },
-  });
+  userId: { type: String, required: true },
+  name: { type: String, required: true, maxlength: [20, 'Nombre de caractéres limité a 15'] },
+  manufacturer: { type: String, required: true, maxlength: [30, 'Nombre de caractéres limité a 30'] },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true, maxlength: [30, 'Nombre de caractéres limité a 15'] },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true },
+  likes: { type: Number, required: false, default: 0 },
+  dislikes: { type: Number, required: false, default: 0 },
+  usersLiked: { type: [String], required: false },
+  usersDisliked: { type: [String], required: false },
+});
 
-  module.exports = mongoose.model('Sauce', sauceSchema);
+module.exports = mongoose.model('Sauce', sauceSchema);
